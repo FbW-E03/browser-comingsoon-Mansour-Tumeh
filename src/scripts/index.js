4a28efd00db4e92a51caf4795d3539e1ed1d2aad
Mansour-Tumeh <mansour.tumeh@digitalcareerinstitute.org>
2021-09-01 16:07:11 +0200

    parcel config

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,5 @@
-// The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
-// Import any additional modules you want to include below \/
+import moment from 'moment';
 
 
-// \/ All of your javascript should go here \/
-
+console.log(moment().format('MMMM Do YYYY, hh:mm:ss a') ) // September 1st 2021, 3:45:28 pm);
